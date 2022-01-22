71652a645bc3cd775a9c6cae9ac1a9989969f5c8
Etienne M <em85@pm.me>
2022-01-22 22:20:36 +0100

    update three react wrapper

--- tools/ThreeReactWrapper.tsx
+++ tools/ThreeReactWrapper.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { isMobile } from '../utils/misc';
 // import { AdvancedThreeApp, ThreeApp, ThreeDemoApp } from '../three-core-modules/core/ThreeApp';
-import './ThreeAppWrapper.css';
+import './ThreeReactWrapper.css';
 import { TouchControls } from './TouchControls';
 
-export let demo: any// demo should inherit of ThreeApp, AdvancedThreeApp, or ThreeDemoApp types
+export let AppClass: any// appInstance should inherit of ThreeApp, AdvancedThreeApp, or ThreeDemoApp types
 
 /**
  * React wrapper for launching three applications
@@ -13,52 +13,68 @@ export let demo: any// demo should inherit of ThreeApp, AdvancedThreeApp, or Thr
  */
 export const ThreeAppWrapper = ({ appClass }) => {
   const ref: any = useRef();
-  const ref2: any = useRef();
   const [item, setItem] = useState(-1);
   const [mode, setMode] = useState(null);
   const touchRef = useRef()
 
+  AppClass = appClass
   useEffect(() => {
     const stateProps = { setItem, setMode };
     //(demo as any).initState(stateProps);
-    demo = new appClass()
-    demo.init();
-    ref.current.appendChild(demo.renderer.domElement)
-    if (demo.stats) {
-      ref2.current.appendChild(demo.stats.dom);
+    const appInstance = AppClass.instance()
+    appInstance.init();
+    ref.current.appendChild(appInstance.renderer.domElement)
+
+    if (appInstance?.state) {
+      appInstance.state.isMobile = isMobile()
+      console.log("is mobile: " + appInstance.state.isMobile)
     }
-    if (demo?.state) {
-      demo.state.isMobile = isMobile()
-      console.log("is mobile: " + demo.state.isMobile)
-    }
-    demo.onWindowResize();
-    (demo as any).animate()
-    touchRef.current = demo.controls
+    appInstance.onWindowResize();
+    (appInstance as any).animate()
+    touchRef.current = appInstance.controls
     // lock screen in landscapte mode
     window.screen.orientation.lock("landscape").then(val => console.log(val))
 
     // renderer.domElement = canvasRef.current;
-    // demo.renderer.setAnimationLoop(demo.render)
+    // appInstance.renderer.setAnimationLoop(demo.render)
   }, [])
 
   // update state for demo apps
 
-  if (demo?.state) {
-    demo.state.item = item;
-    demo.state.mode = mode;
-  }
-
-  console.log("[DemoAppComp] component update")
+  // if (appInstance?.state) {
+  //   appInstance.state.item = item;
+  //   appInstance.state.mode = mode;
+  // }
 
   return (
     <>
-      <div ref={ref2} className="DemoStats" />
-
       <div ref={ref} className="DemoApp" >
         <TouchControls touchRef={touchRef} />
         {/* {currentBottle && mode === CONTROL_MODES.SELECTED && <OverlayV bottleCfg={currentBottle.config} onClose={closeOverlay} />} */}
       </div>
+      <StatsWidget />
+      <DebugInfos />
     </>
 
   );
+}
+
+/**
+ * FPS stats widget
+ */
+const StatsWidget = () => {
+  const ref: any = useRef();
+
+  useEffect(() => {
+    ref.current.appendChild(AppClass.instance().stats.dom);
+  }, [])
+
+  return (<div ref={ref} className="ThreeStats" />)
+}
+
+/**
+ * On screen debug information
+ */
+const DebugInfos = () => {
+  return (<></>)
 }
